8a72ba1aa5bc3f0c7b5c33d035683691057425a4
Jason Salzman <jason@project41innovations.com>
2018-04-06 13:22:38 -0700

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for react-scrollbar 0.5.0
+// Project: https://github.com/souhe/reactScrollbar
+// Definitions by: Stephen Jelfs <https://github.com/stephenjelfs>
+//                 Jason Salzman <https://github.com/jasonsalzman>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
+
+/// <reference types="react" />
+
+declare module "react-scrollbar" {
+  interface ScrollAreaProps extends React.Props<ScrollArea> {
+      className?: string,
+      style?: React.CSSProperties,
+      speed?: number,
+      contentClassName?: string,
+      contentStyle?: React.CSSProperties,
+      vertical?: boolean,
+      verticalContainerStyle?: React.CSSProperties,
+      verticalScrollbarStyle?: React.CSSProperties,
+      horizontal?: boolean,
+      horizontalContainerStyle?: React.CSSProperties,
+      horizontalScrollbarStyle?: React.CSSProperties,
+      onScroll?: (value: {leftPosition: number, topPosition: number, containerHeight: number, containerWidth: number, realHeight: number, realWidth: number}) => void,
+      contentWindow?: any,
+      ownerDocument?: any,
+      smoothScrolling?: boolean
+      minScrollSize?: number,
+      swapWheelAxes?: boolean
+      stopScrollPropagation?: boolean,
+      focusableTabIndex?: number
+  }
+
+  class ScrollArea extends React.Component<ScrollAreaProps> {}
+
+  export default ScrollArea;
+}
---
+++ react-scrollbar-tests.tsx
@@ -0,0 +1,11 @@
+import ScrollArea from 'react-scrollbar';
+import * as React from 'react';
+
+let scrollArea =  <ScrollArea
+                    speed={0.8}
+                    className="area"
+                    contentClassName="content"
+                    horizontal={false}
+                    >
+                      <div>Some long content.</div>
+                  </ScrollArea>;
